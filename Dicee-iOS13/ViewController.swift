9ee17fbf05ebf5323afb5565bae8390c808d60cc
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2021-08-12 16:07:07 -0500

    Add files via upload

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Shubhayu Shrestha on 08/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+    
+
+}
+
